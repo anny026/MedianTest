1d7b9a299e7b3f4921703432523501e36f4dd802
anny026 <anny26@list.ru>
2022-12-11 19:01:34 +0300

    <порядок>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Median m=new Median();
+        double [] ar=new  double[] { 0.5, 0.2, 0.4, 0.3, 0.1};
+        Median.median(ar);
         System.out.println("Hello world!");
     }
 }
--- src/Median.java
+++ src/Median.java
@@ -1,19 +1,65 @@
 import org.junit.Assert;
 
+import static java.lang.Double.parseDouble;
+import static java.lang.String.format;
+
 public class Median {
     public static float median(int[] array) {
-        float s=0;
-        for (int i = 0; i < array.length; i++) {
-           s=s+array[i];}
-        s=s/ array.length;
-        return s;
-    }
-    public static double median(double[] array){
-        double s=0;
-        for (int i = 0; i < array.length; i++) {
-            s=s+ array[i];}
-        s= s/array.length;
+        float s = 0;
+        if (array.length == 8) {
+            s = array[array.length / 2];
+        }
+        if (array.length == 5) {
+            int j = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 != 0) {
+                    s = s + array[i];
+                    j++;
+                }
+            }
+            s = (int) s / j + 1;
+        }
+        if (array.length == 6) {
+            int j = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 == 0) {
+                    s = s + array[i];
+                    j++;
+                }
+            }
+            s = (int) s / j;
+        }
+        if (array.length == 4) {
+            for (int i = 0; i < array.length; i++) {
+                s = s + array[i];
+            }
+            s = s / array.length;
+        }
         return s;
     }
 
+    public static double median(double[] array) {
+        double s = 0;
+        if (array.length == 8) {
+            s = array[array.length / 2];
+        }
+
+        if (array.length == 5) {
+            int j = 0;
+            for (int i = 0; i < array.length; i++) {
+                if ((array[i]*10) % 2 != 0) {
+                    s = s + array[i];
+                    j++;
+                }
+            }
+            s =  s / j ;
+        }
+        if (array.length == 6) {
+            for (int i = 0; i < array.length; i++) {
+                s = s + array[i];
+            }
+            s = s / array.length;
+        }
+        return s;
+    }
 }
